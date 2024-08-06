35225274fcda4135137a902bc4c79625467045e7
laurasgkadri98 <126669056+laurasgkadri98@users.noreply.github.com>
2024-08-06 08:56:09 +0200

    fix pre-commit errors

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name="your_package_name",
+    name="ansys-allie-flowkit-python",
     version="0.1.0",
-    packages=find_packages(include=['app', 'docker', 'configs']),
+    packages=find_packages(include=["app", "docker", "configs"]),
 )
