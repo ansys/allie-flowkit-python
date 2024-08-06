32f1be7cb02ca9e234d04ac39932cda3f799a7b4
afernand <alex.fernandezluces@ansys.com>
2024-08-06 11:26:16 +0200

    fix: Version getter

--- src/allie/flowkit/__init__.py
+++ src/allie/flowkit/__init__.py
@@ -1,6 +1,6 @@
 """App package responsible for creating the FastAPI app."""
 import importlib.metadata as importlib_metadata
 
-__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+__version__ = importlib_metadata.version(__name__.replace(".", "-") + "-python")
 
 from allie.flowkit.flowkit_service import flowkit_service  # noqa F401
