5418acf33e901224f405b4a055385b1041448377
afernand <alex.fernandezluces@ansys.com>
2024-08-07 09:05:40 +0200

    fix: Adapt tests to new changes

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="ansys-allie-flowkit-python",
-    version="0.1.0",
-    packages=find_packages(include=["app", "docker", "configs"]),
-)
